742463a4faacd13168401b373a0265fa3288526b
Thejasvenan <thejasvenant.22@uom.lk>
2024-12-18 13:20:14 +0530

    Check 1

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://df23-2401-dd00-10-20-7c10-b7d3-5f7e-f3da.ngrok-free.app/api',
+  baseURL: 'http://localhost:5000/api',
 });
 
 export const fetchSensorData = () => API.get('/sensor-data');
